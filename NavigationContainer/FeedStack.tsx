b29ebfebba6290bc5f5bc7245d04c2da15c92274
Sandra-atef25 <85317706+Sandra-atef25@users.noreply.github.com>
2024-01-17 23:08:17 +0200

    Update FeedStack.tsx

--- NavigationContainer/FeedStack.tsx
+++ NavigationContainer/FeedStack.tsx
@@ -1,23 +1,32 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 import FeedScreen from "../Screens/Feed/FeedScreen";
 import FeedDetails from "../Screens/Feed/FeedDetails";
 
+import TVStack from "./FeedScreens/TvStack";
+import MoviesStack from "./FeedScreens/MoviesStack";
+
 function FeedStack() {
-  const Stack = createNativeStackNavigator();
+  const MaterialTop = createMaterialTopTabNavigator();
 
   return (
-    <Stack.Navigator
-      initialRouteName="Feed"
-      screenOptions={{ headerStyle: { backgroundColor: "lightblue" } }}
-    >
-      <Stack.Screen name="Feed" component={FeedScreen} />
-      <Stack.Screen
-        name="FeedDetails"
-        component={FeedDetails}
-        options={{ headerTitle: "Feed Details" }}
-      />
-    </Stack.Navigator>
+      <MaterialTop.Navigator
+        initialRouteName="Movies"
+        screenOptions={{ tabBarStyle: { backgroundColor: "lightblue",} }}
+
+      >
+        {/*<MaterialTop.Screen name="Feed" component={FeedScreen} />
+        <MaterialTop.Screen
+          name="FeedDetails"
+          component={FeedDetails}
+          options={{ tabBarLabel: "Feed Details" }}
+        />*/}
+        <MaterialTop.Screen name ="Movies" component={MoviesStack} options={{tabBarScrollEnabled:false}}/>
+        <MaterialTop.Screen name = "TV" component={TVStack} options={{tabBarScrollEnabled:false}}/>
+
+      </MaterialTop.Navigator>
+   
   );
 }
 export default FeedStack;
